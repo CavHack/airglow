e4e07fc9f98edcfd17e6f6e908567486c2a40aec
Karl Whitford <kw@thevenire.com>
2018-02-16 02:58:17 -0500

    New app
    Source to be programmed

---
+++ src/app.js
@@ -0,0 +1,15 @@
+//proxy wont run outside the repo
+import _ from 'lodash'
+import fs from 'fs';
+
+//we need a path
+import path from 'path';
+
+import proxy from 'express-http-proxy';
+import express from 'express';
+
+//lets build our node modules.
+module.exports = function(app) {
+
+
+}
