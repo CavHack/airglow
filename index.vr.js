4dcbd84198eacbd00bb9f67b0cd8bda0c30d399f
Draco_Au <cevb@protonmail.com>
2018-03-31 04:00:35 -0400

    DashboardVR
    Layout Flex

--- index.vr.js
+++ index.vr.js
@@ -13,4 +13,42 @@ import {
 
 import axios from react-native-axios;
 
-export default class airglowVR extends
+export default class airglowVR extends React.Component {
+  constructor() {
+  super();
+  this.state = {
+    scene: 1,
+    IDs: "",
+    selectedStreamID: "",
+    selectedEnv: "",
+  }
+};
+}
+
+//We need two different scenes.
+render() {
+
+  const scene = this.state.scene;
+  return(
+    <view>
+    { scene == 1 ? (
+      <TitleScene
+        showButton={true};
+        text={"Analyze Cluster"}
+        changeScenes={this.changeScenes.bind(this)}
+        scene = {this.state.scene}
+      />
+    ) : (
+      scene  == 2 ? (
+        <Dashboard
+        changeScenes={this.changeScene.bind(this)}
+        scene = {this.state.scene}
+        />
+      )
+    )}
+    </view>
+  );
+
+};
+
+AppRegistry.registerComponent('airglowVR', ()=> airglowVR);
