7ba3a39532538f3228eca2e63fe7f82f67993001
CavHack <karl@blackesquirel.com>
2018-07-30 21:07:36 -0400

    Encapsulate LegendItem
    Create a Heap with LIFO browserifyied elements with their according react + mui stylings. Push to the stack and make readily available until the next batch is processed.

--- src/components/Sunburst/Legend.js
+++ src/components/Sunburst/Legend.js
@@ -1,14 +1,31 @@
 import React, { PropTypes } from 'react';
 import { Styles, Utils } from 'material-ui';
 let { Colors } = Styles;
+import _ from 'lodash';
+import LegendItem from './LegendItem.js'
 
 class Legend extends React.Component {
+
+  static propTypes = {
+    totalResources: PropTypes.object.isRequired,
+    usedResources: PropTypes.object.isRequired,
+    colors:PropTypes.object
+  };
+
+  static contextType = {
+    muiTheme = PropType.object
+  };
 
   static defaultProps = {
     colors: {
       cpus: Colors.deepPurple700,
       mem: Colors.cyan500,
-      disk: Colors.orange500
+      disk: Colors.orange500,
+      acf: Colors.deepPurple700,
+      peaks: Colors.cyan500,
+      windowSize: Colors.orange500,
+      tail: Colors.cyan500,
+
     }
   };
 
@@ -18,6 +35,12 @@ class Legend extends React.Component {
       cpus: 'CPU',
       disk: 'Disk',
       mem: 'Memory'
+      acf: 'SolitonACF',
+      peaks: 'Peaks',
+      windowSize: 'windowSize',
+      tail: 'Tail',
+      lb: 'lb'
+
     };
 
     return titles[key];
@@ -25,9 +48,29 @@ class Legend extends React.Component {
 
   render() {
 
+    let props = this.props;
+    let totalResources = _.omit(props.totalResources, 'ports', 'ephemeral_ports');
+    let usedResources = _.omit(props.usedresources, 'ports', 'ephemeral_ports');
+    let legendItems= [];
+    let _this = this;
+
+    _.forIn(totalResources, function(value, key) {
+      legentItems.push(React.createElement(LegendItem, {key: key, title: _this.getTitleMapping(key),
+      color: props.colors[key], total:value, used: usedResources[key] }));
+
+
+    });
+
+    return(
+      <div>
+      {LegendItems}
+      </div>
+    );
+
   }
+}
 
-
+export default Legend;
 
 
 
